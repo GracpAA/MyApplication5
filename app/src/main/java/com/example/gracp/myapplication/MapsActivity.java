2df3020d4487f571619486abf2dc1ac2af97017f
GracpAA <gracp97@mail.ru>
2017-10-21 00:10:22 +0200

    Adding image1

---
+++ app/src/main/java/com/example/gracp/myapplication/MapsActivity.java
@@ -0,0 +1,69 @@
+package com.example.gracp.myapplication;
+
+import android.support.v4.app.FragmentActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
+
+public class MapsActivity extends FragmentActivity implements OnMapReadyCallback {
+
+    GoogleMap mMap;
+
+    int a = 0;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_maps);
+        // Obtain the SupportMapFragment and get notified when the map is ready to be used.
+        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
+                .findFragmentById(R.id.map);
+        mapFragment.getMapAsync(this);
+    }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_main,menu);
+        return true;
+    }
+
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int id = item.getItemId();
+        switch (id){
+            case R.id.action_setting1:
+
+               if (a == 0)  {
+                   mMap.setMapType(GoogleMap.MAP_TYPE_HYBRID);
+                   a = 1;
+                   item.setIcon(R.drawable.ic_remove_red_eye_black1_24dp);
+               }
+               else {
+                   mMap.setMapType(GoogleMap.MAP_TYPE_NORMAL);
+                   a = 0;
+                   item.setIcon(R.drawable.ic_remove_red_eye_black_24dp);
+               }
+
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+        mMap = googleMap;
+
+        // Add a marker in Sydney and move the camera
+        LatLng sydney = new LatLng(-34, 151);
+        mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
+        mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
+    }
+}
