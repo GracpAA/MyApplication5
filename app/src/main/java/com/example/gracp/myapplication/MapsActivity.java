ed53674056dc69b3ed7799fd6feca990c6ff2643
GracpAA <gracp97@mail.ru>
2017-11-11 18:24:04 +0200

    startVersion

--- app/src/main/java/com/example/gracp/myapplication/MapsActivity.java
+++ app/src/main/java/com/example/gracp/myapplication/MapsActivity.java
@@ -2,8 +2,15 @@ package com.example.gracp.myapplication;
 
 import android.support.v4.app.FragmentActivity;
 import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextUtils;
+import android.text.TextWatcher;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.ImageButton;
+import android.widget.Toast;
 
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
@@ -16,17 +23,53 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
 
     GoogleMap mMap;
 
-    int a = 0;
+    boolean mapType = true;
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_maps);
-        // Obtain the SupportMapFragment and get notified when the map is ready to be used.
-        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
-                .findFragmentById(R.id.map);
+        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager().findFragmentById(R.id.map);
         mapFragment.getMapAsync(this);
+        final EditText searchText = findViewById(R.id.searchEdit);
+        final ImageButton searchButton = findViewById(R.id.searchButton);
+        searchButton.setEnabled(false);
+
+        searchText.addTextChangedListener(new TextWatcher() {
+
+            @Override
+            public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+                if(!TextUtils.isEmpty(charSequence)) {
+                    searchButton.setEnabled(true);
+                    searchButton.setImageResource(R.drawable.ic_search_black1_24dp);
+                }
+                else {
+                    searchButton.setEnabled(false);
+                    searchButton.setImageResource(R.drawable.ic_search_black_24dp);
+                }
+            }
+
+            @Override
+            public void afterTextChanged(Editable editable) {
+
+            }
+        });
+
+        searchButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(MapsActivity.this,R.string.text_activity, Toast.LENGTH_SHORT).show();
+            }
+        });
     }
+
 
 
     @Override
@@ -41,29 +84,28 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
         int id = item.getItemId();
         switch (id){
             case R.id.action_setting1:
-
-               if (a == 0)  {
+               if (mapType)  {
                    mMap.setMapType(GoogleMap.MAP_TYPE_HYBRID);
-                   a = 1;
+                   mapType = false;
                    item.setIcon(R.drawable.ic_remove_red_eye_black1_24dp);
                }
                else {
                    mMap.setMapType(GoogleMap.MAP_TYPE_NORMAL);
-                   a = 0;
+                   mapType = true;
                    item.setIcon(R.drawable.ic_remove_red_eye_black_24dp);
                }
 
         }
         return super.onOptionsItemSelected(item);
-    }
+}
 
     @Override
     public void onMapReady(GoogleMap googleMap) {
         mMap = googleMap;
-
         // Add a marker in Sydney and move the camera
         LatLng sydney = new LatLng(-34, 151);
         mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
         mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
     }
+
 }
